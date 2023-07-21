e9caf070e9b7048d102248d085ce1fa66b27de33
Ar <106869558+Hungrycoddie@users.noreply.github.com>
2023-07-21 22:31:02 +0530

    Global vs. Local Scope in Functions
    It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.

---
+++ Global vs. Local Scope in Functions.js
@@ -0,0 +1,14 @@
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+
+  // Only change code below this line
+    var outerWear = "Sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+console.log(myOutfit());
+console.log(outerWear);
---
+++ Understanding Undefined Value returned from a Function.js
@@ -0,0 +1,20 @@
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum = sum + 5;
+}
+// Only change code above this line
+
+addThree();
+addFive();
+
+//when you consol.log sum you get 8 
+
+console.log(sum)
