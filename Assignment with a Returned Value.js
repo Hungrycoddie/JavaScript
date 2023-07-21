639a8198c99db5d87b24445e004e7a82caa81e2b
Ar <106869558+Hungrycoddie@users.noreply.github.com>
2023-07-21 23:05:44 +0530

    assignment with return values
    //Call the processArg function with an argument of 7 and
    //assign its return value to the variable processed.

---
+++ Assignment with a Returned Value.js
@@ -0,0 +1,25 @@
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+
+console.log(changed)
+
+//*********//
+
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+//Call the processArg function with an argument of 7 and 
+//assign its return value to the variable processed.
+processed = processArg(7);
+
+
+console.log(processed)
+
