9e1a214b13a42cbd3633a54c13cb6b558e84be1c
Ar <106869558+Hungrycoddie@users.noreply.github.com>
2023-07-21 18:51:27 +0530

    eturn a Value from a Function with Return
    return value from a function using return

---
+++ Return a Value from a Function with Return.js
@@ -0,0 +1,20 @@
+function minusSeven(num) {
+  return num - 7;
+}
+
+console.log(minusSeven(10));
+
+//we can return a value from a function using return statement
+
+function timesfive(num) {
+    return num + 5;
+}
+
+console.log(timesfive(5));
+
+
+
+function muliplyeight(num) {
+    return num * 8;
+}
+console.log(muliplyeight(8));
