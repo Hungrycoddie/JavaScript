e819767e558b417b7fdf38a9a23a201a95ff82ad
Ar <106869558+Hungrycoddie@users.noreply.github.com>
2023-07-21 18:39:13 +0530

    Create Passing Values to Functions with Arguments.js
    paramaters are varaiable that act as place holders for the values that are to be input to a function when it is called

---
+++ Passing Values to Functions with Arguments.js
@@ -0,0 +1,19 @@
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+
+functionWithArgs(10, 5);
+
+
+
+
+// the value need not to be only number it can be anything 
+
