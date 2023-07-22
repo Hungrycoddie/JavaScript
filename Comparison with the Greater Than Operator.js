b0c200bf840c8d3f76447404bb0e26c93f574010
Ar <106869558+Hungrycoddie@users.noreply.github.com>
2023-07-22 14:55:14 +0530

    Create Comparison with the Greater Than Operator.js
    Add the greater than operator to the indicated lines so that the return statements make sense.

---
+++ Comparison with the Greater Than Operator.js
@@ -0,0 +1,24 @@
+function testGreaterThan(val) {
+  if (val > 100) {
+    // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {
+    // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+/*
+5   >  3  // true 
+7   > '3' // true
+2   >  3  // false
+'1' >  9  // false
+
+>GREATER THAN 
+<LESSER THAN 
+*/ 
