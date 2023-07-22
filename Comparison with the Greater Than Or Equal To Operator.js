bfb4552adf90ec4e8cef5fce791b4e1097a65323
Ar <106869558+Hungrycoddie@users.noreply.github.com>
2023-07-22 15:10:48 +0530

    Comparison with the Greater Than Or Equal To Operator
    Add the greater than or equal to operator to the indicated lines so that the return statements make sense.

---
+++ Comparison with the Greater Than Or Equal To Operator.js
@@ -0,0 +1,22 @@
+function testGreaterOrEqual(val) {
+  if (val >= 20) {
+    // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {
+    // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+// Examples
+/* 
+6   >=  6  // true
+7   >= '3' // true
+2   >=  3  // false
+'7' >=  9  // false
+*/
