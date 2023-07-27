d23718b592fceb30d2ad075e03e21b6f088ff25c
Ar <106869558+Hungrycoddie@users.noreply.github.com>
2023-07-27 17:21:07 +0530

    Introducing Else statement.
    Combine the if statements into a single if/else statement.

---
+++ Introducing Else Statements.js
@@ -0,0 +1,35 @@
+/* Example Question 
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  if (val <= 5) {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4); */
+
+
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
